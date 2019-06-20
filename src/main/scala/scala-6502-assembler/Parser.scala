130a3438e5806cebe2ee53cd89e89e51087a2125
Tony Aldridge <tony@dabapps.com>
2019-06-20 14:33:03 +0100

    Switched to tokenizing style

--- src/main/scala/scala-6502-assembler/Parser.scala
+++ src/main/scala/scala-6502-assembler/Parser.scala
@@ -2,11 +2,23 @@ package scala_6502_assembler
 
 import scala.util.parsing.combinator._
 
-case class Comment(text: String)
+sealed trait AssemblerToken
 
-class AssemblerParser extends RegexParsers {
-  def semicolon: Parser[String] = """;""".r ^^ { _.toString }
-  def word: Parser[String] = """[A-Za-z]+""".r ^^ { _.toString }
-  def printable: Parser[String] = """[ -~]+""".r ^^ { _.toString }
-  def comment: Parser[Comment] = semicolon ~ printable ^^ { case _ ~ content => Comment(content) }
+case class COMMENT(text: String) extends AssemblerToken
+case class NUMBER(value: Int) extends AssemblerToken
+case class INSTRUCTION(text: String) extends AssemblerToken
+case class LABEL(text: String) extends AssemblerToken
+case class DIRECTIVE(text: String) extends AssemblerToken
+
+class AssemblerLexer extends RegexParsers {
+  override def skipWhitespace = true
+  override val whiteSpace = "[ \t\r\f]+".r
+
+  def comment: Parser[COMMENT] = ";[ -~]+".r ^^ { str => COMMENT(str) }
+  def number: Parser[NUMBER] = "[0-9]+".r ^^ { str => NUMBER(str.toInt) }
+  def decimal: Parser[NUMBER] = "#[0-9]+".r ^^ { str => NUMBER(str.substring(1).toInt) }
+  def hexadecimal: Parser[NUMBER] = """\$[0-9A-F]+""".r ^^ { str => NUMBER(Integer.parseInt(str.substring(1), 16)) }
+  def instruction: Parser[INSTRUCTION] = "[A-Z]{3}".r ^^ { str => INSTRUCTION(str) }
+  def label: Parser[LABEL] = "[A-Za-z]+".r ^^ { str => LABEL(str) }
+  def directive: Parser[DIRECTIVE] = """\.[A-Z]+""".r ^^ { str => DIRECTIVE(str) }
 }
--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -1,27 +1,92 @@
 import org.scalatest.{FlatSpec, DiagrammedAssertions}
-import scala_6502_assembler.{AssemblerParser, Comment}
+import scala_6502_assembler.{AssemblerLexer, COMMENT, NUMBER, INSTRUCTION, LABEL, DIRECTIVE}
 
 
-class ParserSpec extends FlatSpec with DiagrammedAssertions {
-  behavior of "String Parsing"
+class LexerSpec extends FlatSpec with DiagrammedAssertions {
+  behavior of "Comment Tokenizing"
 
-  it should "passthrough strings" in {
-    val parser = new AssemblerParser
-    val result = parser.parse(parser.word, "Hi")
+  it should "tokenize comments" in {
+    val lexer = new AssemblerLexer
+    val result = lexer.parse(lexer.comment, "; Here is a comment")
     assert { result.successful }
-    assert { result.get == "Hi" }
-  }
-
-  behavior of "Comment Parsing"
-
-  it should "correctly parse comments" in {
-    val parser = new AssemblerParser
-    val result = parser.parse(parser.comment, "; Here is a comment")
-    assert { result.successful }
-    val comment = result.get match {
-      case Comment(string) => Some(string)
+    val internal = result.get match {
+      case COMMENT(string) => Some(string)
       case _ => None
     }
-    assert { comment.get == "Here is a comment" }
+    assert { internal.get == "; Here is a comment" }
+  }
+
+  behavior of "Number Tokenizing"
+
+  it should "tokenize plain numbers" in {
+    val lexer = new AssemblerLexer
+    val result = lexer.parse(lexer.number, "12345")
+    assert { result.successful }
+    val internal = result.get match {
+      case NUMBER(n) => Some(n)
+      case _ => None
+    }
+    assert { internal.get == 12345 }
+  }
+
+  it should "tokenize decimals" in {
+    val lexer = new AssemblerLexer
+    val result = lexer.parse(lexer.decimal, "#12345")
+    assert { result.successful }
+    val internal = result.get match {
+      case NUMBER(n) => Some(n)
+      case _ => None
+    }
+    assert { internal.get == 12345 }
+  }
+
+  it should "tokenize hex" in {
+    val lexer = new AssemblerLexer
+    val result = lexer.parse(lexer.hexadecimal, "$DEAD")
+    assert { result.successful }
+    val internal = result.get match {
+      case NUMBER(n) => Some(n)
+      case _ => None
+    }
+    assert { internal.get == 0xDEAD }
+  }
+
+  behavior of "Instruction tokenizing"
+
+  it should "Tokenize instructions" in {
+    val lexer = new AssemblerLexer
+    val result = lexer.parse(lexer.instruction, "POP")
+    assert { result.successful }
+    val internal = result.get match {
+      case INSTRUCTION(n) => Some(n)
+      case _ => None
+    }
+    assert { internal.get == "POP" }
+  }
+
+  behavior of "Label tokenizing"
+
+  it should "Tokenize labels" in {
+    val lexer = new AssemblerLexer
+    val result = lexer.parse(lexer.label, "Shumliduc")
+    assert { result.successful }
+    val internal = result.get match {
+      case LABEL(n) => Some(n)
+      case _ => None
+    }
+    assert { internal.get == "Shumliduc" }
+  }
+
+  behavior of "Directive tokenizing"
+
+  it should "Tokenize directives" in {
+    val lexer = new AssemblerLexer
+    val result = lexer.parse(lexer.directive, ".BYTE")
+    assert { result.successful }
+    val internal = result.get match {
+      case DIRECTIVE(n) => Some(n)
+      case _ => None
+    }
+    assert { internal.get == ".BYTE" }
   }
 }
